78147b0d391326d8f4305856a66b857365ad6d8b
vamsi chunduru <cvamsikrishna11@gmail.com>
2022-08-13 04:15:29 -0400

    added variables

--- main.tf
+++ main.tf
@@ -6,5 +6,7 @@ provider "aws" {
 module "ec2" {
     # source = "../modules/ec2"
     source = "git::https://github.com/cvamsikrishna11/terraform-modules.git//ec2"
+    instance_type = "t2.small"
+
     
 }
