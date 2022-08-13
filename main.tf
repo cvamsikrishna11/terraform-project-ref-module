958aeab40f91d352dd993e71b70cea8b58de24cf
vamsi chunduru <cvamsikrishna11@gmail.com>
2022-08-13 03:51:28 -0400

    project to utilize the remote repo modules

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+
+}
+
+module "ec2" {
+    # source = "../modules/ec2"
+    source = "git::https://github.com/cvamsikrishna11/terraform-modules.git//ec2"
+    
+}
